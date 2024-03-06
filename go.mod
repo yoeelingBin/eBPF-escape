27d90e2236ffb38df703ed15d778512f41e906b0
yoelmiko <2535367985@qq.com>
2024-03-06 10:29:20 +0000

    init go pkgs

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/yoeelingBin/eBPF-escape
 
 go 1.21.3
+
+require github.com/cilium/ebpf v0.13.2
+
+require (
+	golang.org/x/exp v0.0.0-20230224173230-c95f2b4c22f2 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+)
---
+++ pkg/sshd/backdoor-sshd.go
@@ -0,0 +1,14 @@
+package sshd
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/rlimit"
+)
+
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target amd64 bpf ./c/backdoor.c -- -I../headers
